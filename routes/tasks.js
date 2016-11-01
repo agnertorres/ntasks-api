01eb52e2f4879c459a7350316a45d1e906d93ff8
agnertorres <agnertorres@gmail.com>
2016-11-01 19:53:38 -0200

    .

--- routes/tasks.js
+++ routes/tasks.js
@@ -41,9 +41,17 @@ module.exports = app => {
           });
       })
       .put((req, res) => {
-
+        Tasks.update(req.body, {where: req.params})
+          then.(result => res.sendStatus(204))
+          .catch(error => {
+            res.status(412).json({msg: error.message});
+          });
       })
       .delete((req, res) => {
-
+        Tasks.destroy({where: req.params})
+          .then(result => res.sendStatus(204))
+          .catch(error => {
+            res.status(412).json({msg: error.message});
+          });
       });
 };
